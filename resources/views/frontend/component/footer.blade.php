eeb2866f721d45a69693a6fae721e260264e439a
namvhph39762 <namvhph39762@fpt.edu.vn>
2025-06-06 12:51:51 +0700

    feat: create layout front-end (homepage)

---
+++ resources/views/frontend/component/footer.blade.php
@@ -0,0 +1,92 @@
+<div class="panel-subcribe">
+    <div class="uk-container uk-container-center">
+        <div class="uk-flex uk-flex-middle uk-flex-space-between">
+            <div class="panel-head">
+                <h2 class="heading-3"><span>Đăng ký nhận thông tin</span></h2>
+                <div class="description">Để nhận những thông tin khuyến mãi, chiết khấu hấp dẫn</div>
+            </div>
+            <div class="panel-body">
+                <div class="uk-flex uk-flex-middle">
+                    <form action="" class="uk-form subcribe-form">
+                        <input type="text" name="phone" value="" placeholder="Nhập vào số điện thoại">
+                        <button type="submit" name="submit" value="submit">Đăng ký</button>
+                    </form>
+                    <div class="social">
+                        <div class="uk-flex uk-flex-middle">
+                            <a href="" class="social-item"><i class="fa fa-facebook"></i></a>
+                            <a href="" class="social-item"><i class="fa fa-instagram"></i></a>
+                            <a href="" class="social-item"><i class="fa fa-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="footer">
+    <div class="upper">
+        <div class="uk-container uk-container-center">
+            <div class="footer-information">
+                <div class="footer-logo"><img src="" alt=""></div>
+                <div class="company-name"></div>
+
+                <div class="uk-grid uk-grid-medium">
+
+                    <div class="uk-width-large-1-4">
+                        <div class="footer-contact">
+                            <div class="ft-heading">Thông tin liên hệ</div>
+                            <p>Địa chỉ: </p>
+                            <p>Số điện thoại: </p>
+                            <p>Email: </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright uk-text-center">
+        © Copyright 2024, All Rights Reserved - Design by
+    </div>
+</footer>
+<div class="bottom-support-online">
+    <div class="support-content">
+        <a href="tel:09876543231" class="phone-call-now" rel="nofollow">
+            <i style="background:#d92329" class="fa fa-phone rotate" aria-hidden="true"></i>
+            <div class="animated infinite zoomIn kenit-alo-circle" style="border-color:#d92329"></div>
+            <div class="animated infinite pulse kenit-alo-circle-fill" style="background-color:#d92329"></div>
+            <span style="background:#d92329">Gọi ngay: </span>
+        </a>
+        <a class="mes" href="" target="_blank">
+            <i style="background:#d92329" class="fa fa-comments"></i>
+            <span style="background:#d92329">Chat qua Zalo</span>
+        </a>
+    </div>
+    <a class="btn-support">
+        <i style="background:#d92329" class="fa fa-bell" aria-hidden="true"></i>
+        <div class="animated infinite zoomIn kenit-alo-circle" style="border-color:#d92329"></div>
+        <div class="animated infinite pulse kenit-alo-circle-fill" style="background-color:#d92329"></div>
+    </a>
+</div>
+<div class="fix-bottom uk-position-fixed uk-hidden-large">
+    <div class="uk-grid uk-grid-collapse">
+        <div class="uk-width-2-4">
+            <div class="fix-item">
+                <a href="tel:" class="btn btn-main" target="_blank">
+                    <div class="icon"><i class="fa fa-phone rotate"></i></div>
+                    <div class="text">Gọi điện</div>
+                </a>
+            </div>
+        </div>
+        <div class="uk-width-2-4">
+            <div class="fix-item">
+                <a href="" class="btn btn-main" target="_blank">
+                    <div class="image img-cover"><i class="fa fa-comments"></i></div>
+                    <div class="text">Zalo</div>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
