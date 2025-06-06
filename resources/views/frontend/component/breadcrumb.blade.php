4581ec3cec0b3c2c3117b441a557bc7c40628ae4
namvhph39762 <namvhph39762@fpt.edu.vn>
2025-06-06 12:51:51 +0700

    feat: create layout front-end (homepage)

---
+++ resources/views/frontend/component/breadcrumb.blade.php
@@ -0,0 +1,8 @@
+<div class="page-breadcrumb background">
+    <div class="uk-container uk-container-center">
+        <ul class="uk-list uk-clearfix">
+            <li><a href="/"><i class="fi-rs-home mr5"></i></a></li>
+            <li><a href="" title=""></a></li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/frontend/component/head.blade.php
@@ -0,0 +1,15 @@
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1,user-scalable=0">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta http-equiv="refresh" content="1800"/>
+<link rel="icon" href="" type="image/png" sizes="30x30">
+<!-- GOOGLE -->
+<title>Title}</title>
+
+<meta property="og:locale" content="vi_VN"/>
+
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+<!-- <link href="backend/css/bootstrap.min.css" rel="stylesheet"> -->
+
+<script src="{{ asset('frontend/resources/library/js/jquery.js') }}"></script>
+
