33c17e7a0f01887ccb131a73de6686b5054566f0
haphamfpt <haphph36438@fpt.edu.vn>
2025-06-06 12:05:58 +0700

    feat: create layout product (admin)

---
+++ resources/views/backend/product/category/store.blade.php
@@ -0,0 +1,24 @@
+@include('backend.dashboard.component.breadcrumb')
+@include('backend.dashboard.component.formError')
+
+<form action="" method="post" class="box">
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-9">
+                <div class="ibox">
+                    <div class="ibox-title">
+                        <h5>Title</h5>
+                    </div>
+                    <div class="ibox-content">
+                        @include('backend.dashboard.component.content')
+                    </div>
+                </div>
+                @include('backend.dashboard.component.album')
+            </div>
+            <div class="col-lg-3">
+                @include('backend.product.catalogue.component.aside')
+            </div>
+        </div>
+        @include('backend.dashboard.component.button')
+    </div>
+</form>
