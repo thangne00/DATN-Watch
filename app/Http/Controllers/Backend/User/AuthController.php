e4ab8811009037e255533e47124f21343f739230
thangndph37125 <thangndph37125@fpt.edu.vn>
2025-06-09 13:23:13 +0700

    login, logout (admin)

---
+++ app/Http/Controllers/Backend/User/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Backend\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+        if (Auth::id() > 0) {
+            return redirect()->route('dashboard.index');
+        }
+
+        return view('backend.auth.login');
+    }
+
+    public function login(AuthRequest $request)
+    {
+        $credentials = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ];
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('dashboard.index')->with('success', 'Đăng nhập thành công');
+        }
+        return redirect()->route('auth.admin')->with('error', 'Email hoặc Mật khẩu không chính xác');
+
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('auth.admin');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\User\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('admin', [AuthController::class, 'index'])->name('auth.admin')->middleware('login');
+Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
 
 Route::get('/', function () {
     return view('welcome');
