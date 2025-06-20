f933e9366e147850c5a5f67a809faea8586b57bc
namvhph39762 <namvhph39762@fpt.edu.vn>
2025-06-20 13:08:55 +0700

    sua va xoa danh muc bai viet

--- resources/views/backend/post/catalogue/component/filter.blade.php
+++ resources/views/backend/post/catalogue/component/filter.blade.php
@@ -1,4 +1,4 @@
-<form action="">
+<form action="{{ route('post.catalogue.index') }}">
     <div class="filter-wrapper">
         <div class="uk-flex uk-flex-middle uk-flex-space-between">
             @include('backend.dashboard.component.perpage')
@@ -6,7 +6,7 @@
                 <div class="uk-flex uk-flex-middle">
                     @include('backend.dashboard.component.filterPublish')
                     @include('backend.dashboard.component.keyword')
-                    <a href="" class="btn btn-danger"><i class="fa fa-plus mr5"></i></a>
+                    <a href="{{ route('post.catalogue.create') }}" class="btn btn-danger"><i class="fa fa-plus mr5"></i>{{ __('messages.postCatalogue.create.title') }}</a>
                 </div>
             </div>
         </div>
--- resources/views/backend/post/catalogue/delete.blade.php
+++ resources/views/backend/post/catalogue/delete.blade.php
@@ -1,5 +1,13 @@
-@include('backend.dashboard.component.breadcrumb')
-
-<form action="" method="post" class="box">
-    @include('backend.dashboard.component.destroy')
+@include('backend.dashboard.component.breadcrumb', ['title' => $config['seo']['delete']['title']])
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<form action="{{ route('post.catalogue.destroy', $postCatalogue->id) }}" method="post" class="box">
+    @include('backend.dashboard.component.destroy', ['model' => ($postCatalogue) ?? null])
 </form>
