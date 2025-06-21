bd360f7713f50214df19637ce7b6d008c498f9f5
haphamfpt <haphph36438@fpt.edu.vn>
2025-06-21 12:24:00 +0700

    danh sach va them moi bai viet

--- resources/views/backend/post/post/store.blade.php
+++ resources/views/backend/post/post/store.blade.php
@@ -1,20 +1,31 @@
-@include('backend.dashboard.component.breadcrumb')
-
-<form action="" method="" class="box">
-
+@include('backend.dashboard.component.breadcrumb', ['title' => $config['seo'][$config['method']]['title']])
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+@php
+    $url = ($config['method'] == 'create') ? route('post.store') : route('post.update', $post->id);
+@endphp
+<form action="{{ $url }}" method="post" class="box">
+    @csrf
     <div class="wrapper wrapper-content animated fadeInRight">
         <div class="row">
             <div class="col-lg-9">
                 <div class="ibox">
                     <div class="ibox-title">
-                        <h5>messages</h5>
+                        <h5>{{ __('messages.tableHeading') }}</h5>
                     </div>
                     <div class="ibox-content">
-                        @include('backend.dashboard.component.content')
+                        @include('backend.dashboard.component.content', ['model' => ($post) ?? null])
                     </div>
                 </div>
                @include('backend.dashboard.component.album')
-               @include('backend.dashboard.component.seo')
+               @include('backend.dashboard.component.seo', ['model' => ($post) ?? null])
             </div>
             <div class="col-lg-3">
                 @include('backend.post.post.component.aside')
