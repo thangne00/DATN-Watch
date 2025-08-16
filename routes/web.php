c756c3f6bbe82763075ed140696dcbb811eaa139
namvhph39762 <namvhph39762@fpt.edu.vn>
2025-08-16 13:21:22 +0700

    create class momo

--- routes/web.php
+++ routes/web.php
@@ -37,9 +37,6 @@ use App\Http\Controllers\Frontend\CustomerController as FeCustomerController;
 |
 */
 
-
-
-
 Route::get('admin', [AuthController::class, 'index'])->name('auth.admin')->middleware('login');
 Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
 Route::post('login', [AuthController::class, 'login'])->name('auth.login');
