ea85e24e52107bcf74e601b0aa969982d374ca89
huynvph43256 <huynvph43256@fpt.edu.vn>
2025-08-17 13:11:33 +0700

    construct

--- resources/views/backend/construct/index.blade.php
+++ resources/views/backend/construct/index.blade.php
@@ -1,15 +1,14 @@
-
 @include('backend.dashboard.component.breadcrumb', ['title' => $config['seo']['index']['title']])
 <div class="row mt20">
     <div class="col-lg-12">
         <div class="ibox float-e-margins">
             <div class="ibox-title">
                 <h5>{{ $config['seo']['index']['table']; }} </h5>
-                @include('backend.dashboard.component.toolbox', ['model' => 'AttributeCatalogue'])
+                @include('backend.dashboard.component.toolbox', ['model' => 'Construction'])
             </div>
             <div class="ibox-content">
-                @include('backend.attribute.catalogue.component.filter')
-                @include('backend.attribute.catalogue.component.table')
+                @include('backend.construct.component.filter')
+                @include('backend.construct.component.table')
             </div>
         </div>
     </div>
