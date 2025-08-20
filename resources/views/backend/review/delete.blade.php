753c30477d33d3fa2e2765ec493e950110fe414b
haphamfpt <haphph36438@fpt.edu.vn>
2025-08-20 22:41:34 +0700

    update

--- resources/views/backend/review/delete.blade.php
+++ resources/views/backend/review/delete.blade.php
@@ -10,7 +10,8 @@
                     <div class="panel-title">Thông tin chung</div>
                     <div class="panel-description">
                         <p>Bạn đang muốn xóa bản ghi: {{ $widget->email }}</p>
-                        <p>Lưu ý: Không thể khôi phục bản ghi sau khi xóa. Hãy chắc chắn bạn muốn thực hiện chức năng này</p>
+                        <p>Lưu ý: Không thể khôi phục bản ghi sau khi xóa nhé. Hãy chắc chắn bạn muốn thực hiện chức
+                            năng này</p>
                     </div>
                 </div>
             </div>
@@ -20,16 +21,10 @@
                         <div class="row mb15">
                             <div class="col-lg-12">
                                 <div class="form-row">
-                                    <label for="" class="control-label text-left">Tên Bản ghi <span class="text-danger">(*)</span></label>
-                                    <input 
-                                        type="text"
-                                        name="name"
-                                        value="{{ old('name', ($widget->name) ?? '' ) }}"
-                                        class="form-control"
-                                        placeholder=""
-                                        autocomplete="off"
-                                        readonly
-                                    >
+                                    <label for="" class="control-label text-left">Tên Bản ghi <span
+                                            class="text-danger">(*)</span></label>
+                                    <input type="text" name="name" value="{{ old('name', ($widget->name) ?? '') }}"
+                                        class="form-control" placeholder="" autocomplete="off" readonly>
                                 </div>
                             </div>
                         </div>
@@ -37,7 +32,7 @@
                 </div>
             </div>
         </div>
-       
+
         <div class="text-right mb15">
             <button class="btn btn-danger" type="submit" name="send" value="send">Xóa dữ liệu</button>
         </div>
