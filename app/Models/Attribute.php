e8a5bb26f2317c89a62546e386096c55686b7350
anhvvph42683 <anhvvph42683@fpt.edu.vn>
2025-08-25 00:56:20 +0700

    sua loi

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Validation\Rule;
 use App\Traits\QueryScopes;
 
 class Attribute extends Model
@@ -24,30 +23,35 @@ class Attribute extends Model
 
     protected $table = 'attributes';
 
-    public function languages(){
-        return $this->belongsToMany(Language::class, 'attribute_language' , 'attribute_id', 'language_id')
-        ->withPivot(
-            'name',
-            'canonical',
-            'meta_title',
-            'meta_keyword',
-            'meta_description',
-            'description',
-            'content'
-        )->withTimestamps();
-    }
-    public function attribute_catalogues(){
-        return $this->belongsToMany(AttributeCatalogue::class, 'attribute_catalogue_attribute' , 'attribute_id', 'attribute_catalogue_id');
+    public function languages()
+    {
+        return $this->belongsToMany(Language::class, 'attribute_language', 'attribute_id', 'language_id')
+            ->withPivot(
+                'name',
+                'canonical',
+                'meta_title',
+                'meta_keyword',
+                'meta_description',
+                'description',
+                'content'
+            )
+            ->withTimestamps();
     }
 
-    public function attribute_language(){
+    public function attribute_catalogues()
+    {
+        return $this->belongsToMany(AttributeCatalogue::class, 'attribute_catalogue_attribute', 'attribute_id', 'attribute_catalogue_id');
+    }
+
+    public function attribute_languages()
+    {
         return $this->hasMany(AttributeLanguage::class, 'attribute_id');
     }
 
-    public function product_variants(){
-        return  $this->belongsToMany(ProductVariant::class, 'product_variant_attribute' , 'attribute_id', 'attribute_id')
-        ->withPivot(
-            'name',
-        )->withTimestamps();
+    public function product_variants()
+    {
+        return $this->belongsToMany(ProductVariant::class, 'product_variant_attribute', 'attribute_id', 'product_variant_id')
+            ->withPivot('name')
+            ->withTimestamps();
     }
 }
